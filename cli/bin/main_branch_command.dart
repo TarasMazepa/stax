c81332cbef88bb4f477517311230bbc448ad0fc4
Taras Mazepa <git@okl.pw>
2023-09-17 13:02:52 -0700

    Finishes parsing of main branch

--- cli/bin/main_branch_command.dart
+++ cli/bin/main_branch_command.dart
@@ -16,7 +16,8 @@ class MainBranchCommand extends Command {
         .stdout
         .toString()
         .split("\n")
-        .map((e) => e.trim());
+        .map((e) => e.trim())
+        .where((element) => element.isNotEmpty);
     final String remote;
     switch (remotes.length) {
       case 0:
@@ -28,6 +29,15 @@ class MainBranchCommand extends Command {
         print("You have many remotes. I will just pick the first one.");
         remote = remotes.first;
     }
-
+    final defaultBranch = Git.revParseAbbrevRef
+        .withArgument("$remote/HEAD")
+        .announce("Checking default branch on remote")
+        .runSync()
+        .printNotEmptyResultFields()
+        .stdout
+        .toString()
+        .trim()
+        .split("/")[1];
+    print("Your main branch is $defaultBranch.");
   }
 }
