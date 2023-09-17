e5a55ecdd49a5880f49d4ea9e82faadb3c880e77
Taras Mazepa <git@okl.pw>
2023-09-16 17:37:59 -0700

    Refactor on code units

--- cli/lib/code_units.dart
+++ cli/lib/code_units.dart
@@ -1,14 +1,20 @@
 import 'package:stax/int_range.dart';
 
-extension CodeUnitOnInt on int {
+class CodeUnits {
   static final smallLetterRange =
       IntRange.closed("a".codeUnitAt(0), "z".codeUnitAt(0));
   static final largeLetterRange =
       IntRange.closed("A".codeUnitAt(0), "Z".codeUnitAt(0));
   static final numbersRange =
       IntRange.closed("0".codeUnitAt(0), "9".codeUnitAt(0));
+
   static final dot = ".".codeUnitAt(0);
   static final dash = "-".codeUnitAt(0);
   static final underscore = "_".codeUnitAt(0);
   static final slash = "/".codeUnitAt(0);
+
+  static final dotRange = IntRange.singleton(dot);
+  static final dashRange = IntRange.singleton(dash);
+  static final underscoreRange = IntRange.singleton(underscore);
+  static final slashRange = IntRange.singleton(slash);
 }
