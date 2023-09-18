3f1c91c4d2ac097e8a55c63ba024149e3bae115e
Taras Mazepa <git@okl.pw>
2023-09-17 17:51:40 -0700

    Introduces factory method for external commands in Git class

--- cli/lib/git.dart
+++ cli/lib/git.dart
@@ -1,23 +1,27 @@
 import 'external_command.dart';
 
 class Git {
-  late final branch = ExternalCommand.raw("git branch");
+  late final branch = rawEc("git branch");
   late final branchCurrent = branch.arg("--show-current");
   late final branchDelete = branch.arg("-D");
   late final branchVv = branch.arg("-vv");
-  late final checkout = ExternalCommand.raw("git checkout");
+  late final checkout = rawEc("git checkout");
   late final checkoutNewBranch = checkout.arg("-b");
-  late final commit = ExternalCommand.raw("git commit");
+  late final commit = rawEc("git commit");
   late final commitWithMessage = commit.arg("-m");
-  late final diff = ExternalCommand.raw("git diff");
+  late final diff = rawEc("git diff");
   late final diffCachedQuiet = diff.args(["--cached", "--quiet"]);
-  late final fetch = ExternalCommand.raw("git fetch");
+  late final fetch = rawEc("git fetch");
   late final fetchWithPrune = fetch.arg("-p");
-  late final pull = ExternalCommand.raw("git pull");
-  late final push = ExternalCommand.raw("git push");
-  late final revList = ExternalCommand.raw("git rev-list");
+  late final pull = rawEc("git pull");
+  late final push = rawEc("git push");
+  late final revList = rawEc("git rev-list");
   late final revListCount = revList.arg("--count");
-  late final remote = ExternalCommand.raw("git remote");
-  late final revParse = ExternalCommand.raw("git rev-parse");
+  late final remote = rawEc("git remote");
+  late final revParse = rawEc("git rev-parse");
   late final revParseAbbrevRef = revParse.arg("--abbrev-ref");
+
+  ExternalCommand rawEc(String raw) {
+    return ExternalCommand.raw(raw);
+  }
 }
