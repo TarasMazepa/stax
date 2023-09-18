045edb27009d6550c21f8367950fb5901efbf843
Taras Mazepa <git@okl.pw>
2023-09-17 19:37:18 -0700

    Adds context for subcommands of amend

--- cli/bin/internal_command_amend.dart
+++ cli/bin/internal_command_amend.dart
@@ -6,15 +6,22 @@ class InternalCommandAmend extends InternalCommand {
 
   @override
   void run(ContextForInternalCommand context) {
-    if (context.git.diffCachedQuiet.runSync().exitCode == 0) {
+    if (context.git.diffCachedQuiet
+            .announce("Checking if there staged changes.")
+            .runSync()
+            .exitCode ==
+        0) {
       context.printToConsole("Can't amend - there is nothing staged. "
           "Run 'git add .' to add all the changes.");
       return;
     }
     context.git.commitAmendNoEdit
-        .announce()
+        .announce("Amending changes to a commit.")
         .runSync()
         .printNotEmptyResultFields();
-    context.git.pushForce.announce().runSync().printNotEmptyResultFields();
+    context.git.pushForce
+        .announce("Force pushing to a remote.")
+        .runSync()
+        .printNotEmptyResultFields();
   }
 }
