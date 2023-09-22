25bcda92b18b5bee67652a67c4341305d9926cee
Taras Mazepa <git@okl.pw>
2023-09-22 13:06:30 -0700

    Adds settings internal command which now just prints settings

---
+++ cli/bin/internal_command_settings.dart
@@ -0,0 +1,18 @@
+import 'package:stax/context_for_internal_command.dart';
+import 'package:stax/settings/settings.dart';
+
+import 'internal_command.dart';
+import 'types_for_internal_command.dart';
+
+class InternalCommandSettings extends InternalCommand {
+  InternalCommandSettings()
+      : super("settings", "View stax settings",
+            type: InternalCommandType.hidden);
+
+  @override
+  void run(ContextForInternalCommand context) {
+    for (final entry in Settings.instance.settings.entries) {
+      context.printToConsole("${entry.key}: ${entry.value}");
+    }
+  }
+}
--- cli/bin/internal_commands.dart
+++ cli/bin/internal_commands.dart
@@ -4,6 +4,7 @@ import 'internal_command_available_commands.dart';
 import 'internal_command_commit.dart';
 import 'internal_command_delete_gone_branches.dart';
 import 'internal_command_main_branch.dart';
+import 'internal_command_settings.dart';
 import 'internal_command_terminal.dart';
 import 'internal_command_update.dart';
 import 'internal_command_update_prompt.dart';
@@ -17,6 +18,7 @@ final List<InternalCommand> internalCommands = [
   InternalCommandMainBranch(),
   InternalCommandAmend(),
   InternalCommandUpdatePrompt(),
+  InternalCommandSettings(),
 ]..sort();
 
 final Map<String, InternalCommand> internalCommandRegistry = {
