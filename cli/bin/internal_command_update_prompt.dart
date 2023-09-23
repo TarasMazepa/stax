f6cbb21e2ee3d85334fdfa37e5bc04ff0b0e8dc8
Taras Mazepa <git@okl.pw>
2023-09-22 18:53:21 -0700

    Adds a bit of implementation for InternalCommandUpdatePrompt

--- cli/bin/internal_command_update_prompt.dart
+++ cli/bin/internal_command_update_prompt.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:stax/context_for_internal_command.dart';
+import 'package:stax/file_path_dir_on_uri.dart';
+import 'package:stax/settings/settings.dart';
+import 'package:stax/shortcuts_for_internal_command_context.dart';
 
 import 'internal_command.dart';
 import 'types_for_internal_command.dart';
@@ -9,5 +14,16 @@ class InternalCommandUpdatePrompt extends InternalCommand {
             type: InternalCommandType.hidden);
 
   @override
-  void run(ContextForInternalCommand context) {}
+  void run(ContextForInternalCommand context) {
+    final lastUpdatePrompt = Settings.instance.lastUpdatePrompt.get();
+    final now = DateTime.now();
+    final silenceDuration = Duration(days: 1);
+    if (lastUpdatePrompt.add(silenceDuration).isAfter(now)) return;
+    Settings.instance.lastUpdatePrompt.set(now);
+    final silentContext = ContextForInternalCommand.silent();
+    final myDir = Platform.script.toFilePathDir();
+    if (!silentContext.isCurrentBranchBehind(workingDirectory: myDir)) {
+      // fetch
+    }
+  }
 }
