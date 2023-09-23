d3651925f8673499610421cfd24a4f5d17c8cac6
Taras Mazepa <git@okl.pw>
2023-09-23 16:58:01 -0700

    Removes not used count on list

--- cli/lib/count_on_list.dart
+++
@@ -1,9 +0,0 @@
-extension CountOnList<T> on List<T> {
-  int count(T item) {
-    int result = 0;
-    for (var value in this) {
-      if (value == item) result++;
-    }
-    return result;
-  }
-}
