03cf0dd2d0b0139a691e905a05d63259f8f2bcff
Taras Mazepa <git@okl.pw>
2023-09-30 09:11:38 -0700

    Refactoring InternalCommandMainBranch to support many remotes and better checking for success as well as explanations to failure and some hints how to fix it yourself.

--- cli/bin/internal_command_main_branch.dart
+++ cli/bin/internal_command_main_branch.dart
@@ -1,7 +1,8 @@
 import 'package:stax/context/context.dart';
+import 'package:stax/on_empty_on_iterable.dart';
+import 'package:stax/once.dart';
 
 import 'internal_command.dart';
-import 'internal_command_stopped_execution_exception.dart';
 import 'types_for_internal_command.dart';
 
 class InternalCommandMainBranch extends InternalCommand {
@@ -9,8 +10,9 @@ class InternalCommandMainBranch extends InternalCommand {
       : super("main-branch", "Shows which branch stax considers to be main.",
             type: InternalCommandType.hidden);
 
-  String getDefaultBranchName(final List<String> args, final Context context) {
-    final remotes = context.git.remote
+  String? getDefaultBranch(final List<String> args, final Context context) {
+    final emptyComplain = Once();
+    return context.git.remote
         .announce("Checking name of your remote.")
         .runSync()
         .printNotEmptyResultFields()
@@ -19,18 +21,13 @@ class InternalCommandMainBranch extends InternalCommand {
         .split("\n")
         .map((e) => e.trim())
         .where((element) => element.isNotEmpty)
-        .toList();
-    if (remotes.isEmpty) {
-      context.printToConsole(
-          "You have no remotes. Can't figure out default branch.");
-      throw InternalCommandStoppedExecutionException();
-    }
-    final defaultBranches = remotes
+        .onEmpty(emptyComplain.wrap(() => context.printToConsole(
+            "You have no remotes. Can't figure out default branch.")))
         .map((remote) => (
               remote: remote,
               parts: context.git.revParseAbbrevRef
                   .arg("$remote/HEAD")
-                  .announce("Checking default branch on '$remote' remote")
+                  .announce("Checking default branch on '$remote' remote.")
                   .runSync()
                   .printNotEmptyResultFields()
                   .stdout
@@ -41,23 +38,15 @@ class InternalCommandMainBranch extends InternalCommand {
         .where((e) => e.parts.length == 2)
         .where((e) => e.remote == e.parts[0])
         .map((e) => e.parts[1])
-        .toSet()
-        .toList();
-    final defaultBranch = switch (defaultBranches) {
-      [] => throw InternalCommandStoppedExecutionException(),
-      [final one] => one,
-      _ => defaultBranches.first
-    };
-    context.printToConsole("Your main branch is $defaultBranch.");
-    return defaultBranch;
+        .onEmpty(emptyComplain.wrap(() => context.printToConsole(
+            "None of your remotes have '<remote>/HEAD' ref locally. Try adding one manually. Couldn't figure out default branch.")))
+        .firstOrNull;
   }
 
   @override
   void run(final List<String> args, final Context context) {
-    try {
-      getDefaultBranchName(args, context);
-    } on InternalCommandStoppedExecutionException {
-      return;
-    }
+    final defaultBranch = getDefaultBranch(args, context);
+    if (defaultBranch == null) return;
+    context.printToConsole("Your default branch is '$defaultBranch'");
   }
 }
