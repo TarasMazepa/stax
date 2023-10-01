52e1437f24fccead34b551cda903796d727145fe
Taras Mazepa <git@okl.pw>
2023-10-01 14:38:12 -0700

    Adds basic logic for building default branch trunk

--- cli/bin/internal_command_log.dart
+++ cli/bin/internal_command_log.dart
@@ -1,5 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:stax/context/context.dart';
 import 'package:stax/context/context_git_get_all_branches.dart';
+import 'package:stax/context/context_git_get_default_branch.dart';
 
 import 'internal_command.dart';
 
@@ -8,6 +10,26 @@ class InternalCommandLog extends InternalCommand {
 
   @override
   void run(List<String> args, Context context) {
-    context.printToConsole(context.getAllBranches().join("\n"));
+    final defaultBranchName = context.getDefaultBranch();
+    if (defaultBranchName == null) {
+      return;
+    }
+    final allBranches = context.getAllBranches();
+    final defaultBranch =
+        allBranches.where((e) => e.name == defaultBranchName).firstOrNull;
+    if (defaultBranch == null) {
+      return;
+    }
+    allBranches.remove(defaultBranch);
+    final connectionPoints = groupBy(
+        allBranches,
+        (branch) => context.git.mergeBase
+            .args([defaultBranch.name, branch.name])
+            .runSync()
+            .printNotEmptyResultFields()
+            .stdout
+            .toString()
+            .trim());
+    context.printToConsole(connectionPoints);
   }
 }
