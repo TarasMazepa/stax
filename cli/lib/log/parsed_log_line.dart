926733d29a6c70d20a4d5ce04564aa4ae6e13781
Taras Mazepa <git@okl.pw>
2023-10-07 11:25:08 -0700

    Adds branch indexes

--- cli/lib/log/parsed_log_line.dart
+++ cli/lib/log/parsed_log_line.dart
@@ -1,9 +1,14 @@
 class ParsedLogLine {
+  final Set<int> branchIndexes;
   final String pattern;
   final String commitHash;
   final String commitMessage;
 
-  ParsedLogLine(this.pattern, this.commitHash, this.commitMessage);
+  ParsedLogLine(this.pattern, this.commitHash, this.commitMessage)
+      : branchIndexes = pattern.codeUnits.indexed
+            .where((e) => e.$2 != " ".codeUnits[0])
+            .map((e) => e.$1)
+            .toSet();
 
   factory ParsedLogLine.parse(String line) {
     final leftBracketIndex = line.indexOf("[");
