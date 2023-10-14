db5d24a2f83c7878dfc61c214d5bfecb35d1e95e
Taras Mazepa <git@okl.pw>
2023-10-14 11:26:15 -0700

    Put suggested git command into single quotes

--- cli/lib/context/context_explain_to_user_no_staged_changes.dart
+++ cli/lib/context/context_explain_to_user_no_staged_changes.dart
@@ -5,8 +5,8 @@ extension ContextExplainToUserNoStagedChanges on Context {
     printToConsole("Can't commit - there is nothing staged. "
         "You can add -a flag next time to add all the files. "
         "Or run 'git add .' to add all the changes. "
-        "Or add individual files using git add <filename>. "
-        "For more information see git add --help . "
+        "Or add individual files using 'git add <filename>'. "
+        "For more information see 'git add --help'. "
         "If that haven't worked - try editing some files.");
   }
 }
