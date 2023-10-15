962a105e969f6d0cb9a7d4ffb5b2950a0e188c03
Taras Mazepa <git@okl.pw>
2023-10-15 14:39:59 -0700

    Adds cliGroup to hold group of cli tests with automatic set up and tear down, which setups repo from bundle and deletes it.

---
+++ cli/test/cli/base/cli_group.dart
@@ -0,0 +1,31 @@
+import 'package:meta/meta.dart';
+import 'package:test/scaffolding.dart';
+
+import 'cli_test_setup.dart';
+
+@isTestGroup
+void cliGroup(Object? description, dynamic Function(String path) body,
+    {String? testOn,
+    Timeout? timeout,
+    Object? skip,
+    Object? tags,
+    Map<String, dynamic>? onPlatform,
+    int? retry,
+    @Deprecated('Debug only') bool solo = false}) {
+  final CliTestSetup setup = CliTestSetup.create();
+  group(description,
+      testOn: testOn,
+      timeout: timeout,
+      skip: skip,
+      tags: tags,
+      onPlatform: onPlatform,
+      retry: retry, () {
+    setUp(() {
+      setup.setUp();
+    });
+    tearDown(() {
+      setup.tearDown();
+    });
+    body(setup.testRepoPath);
+  });
+}
