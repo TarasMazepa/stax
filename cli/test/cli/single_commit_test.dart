e4c1769967c199a89ea5e66bcdf9ae480c48e2ea
Taras Mazepa <git@okl.pw>
2023-10-15 19:26:47 -0700

    Adds single_commit_test.dart "log --collapse" and "log --branches" tests

--- cli/test/cli/single_commit_test.dart
+++ cli/test/cli/single_commit_test.dart
@@ -12,5 +12,15 @@ void main() {
       expect(setup.runLiveStaxSync(["log"]).stdout.toString().trim(),
           "* [465015f] main Adds readme");
     });
+    test("log --collapse", () {
+      expect(
+          setup.runLiveStaxSync(["log", "--collapse"]).stdout.toString().trim(),
+          "* [465015f] main Adds readme");
+    });
+    test("log --branches", () {
+      expect(
+          setup.runLiveStaxSync(["log", "--branches"]).stdout.toString().trim(),
+          "* main");
+    });
   });
 }
