f42825f4a06004536e3026aab490e1d86b51495a
Alvee <M.Alvee8141@gmail.com>
2024-05-12 18:38:00 +0600

    Update string_empty_to_null_test.dart

--- cli/test/string_empty_to_null_test.dart
+++ cli/test/string_empty_to_null_test.dart
@@ -2,8 +2,14 @@ import "package:stax/string_empty_to_null.dart";
 import "package:test/test.dart";
 
 void main() {
-  test("Test if empty string becomes null", () {
+  test("Test if empty string returns null", () {
     String emptyString = "";
     expect(emptyString.emptyToNull(), null);
+  });
+
+  test("Test if null string returns null", () {
+    String? nullString;
+
+    expect(nullString, null);
   });
 }
