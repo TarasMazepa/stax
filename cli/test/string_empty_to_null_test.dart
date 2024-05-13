2871b606240690c73e42c57352fdcb35972910eb
Alvee <M.Alvee8141@gmail.com>
2024-05-13 09:55:48 -0700

    test for string_empty_to_null (#36)
    * test for string_empty_to_null
    
    * Update string_empty_to_null_test.dart
    
    * review changes

---
+++ cli/test/string_empty_to_null_test.dart
@@ -0,0 +1,18 @@
+import "package:stax/string_empty_to_null.dart";
+import "package:test/test.dart";
+
+void main() {
+  test("empty string", () {
+    expect("".emptyToNull(), null);
+  });
+
+  test("null", () {
+    String? nullString = null;
+
+    expect(nullString.emptyToNull(), null);
+  });
+
+  test("nonempty string", () {
+    expect("something".emptyToNull(), "something");
+  });
+}
