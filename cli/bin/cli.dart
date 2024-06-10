7ad4666aaec9720498c44decc09bee64968795f2
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-06-10 13:45:16 -0700

    Removes update prompt (#46)
    Co-authored-by: Taras Mazepa <taras@daylightcomputer.com>

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -2,15 +2,11 @@ import 'package:stax/context/context.dart';
 import 'package:stax/context/context_handle_global_flags.dart';
 
 import 'internal_command_available_commands.dart';
-import 'internal_command_update.dart';
-import 'internal_command_update_prompt.dart';
 import 'internal_commands.dart';
 
 void main(List<String> arguments) {
   arguments = arguments.toList();
   final context = Context.implicit().handleGlobalFlags(arguments);
-  bool shouldUpdate = InternalCommandUpdatePrompt()
-      .shouldAutoUpdateAfterExecutingCommand([], context.withSilence(true));
   switch (arguments) {
     case []:
       InternalCommandAvailableCommands().run([], context);
@@ -21,8 +17,5 @@ void main(List<String> arguments) {
         return;
       }
       command.run(args, context);
-  }
-  if (shouldUpdate) {
-    InternalCommandUpdate().run([], context);
   }
 }
