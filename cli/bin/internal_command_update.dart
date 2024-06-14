b871e33f4bc0c0c6c05e8c5624db2ee5fe8c7ad4
Taras Mazepa <taras@daylightcomputer.com>
2024-06-14 07:21:39 -0700

    Returns update command to be working on windows

--- cli/bin/internal_command_update.dart
+++ cli/bin/internal_command_update.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:stax/context/context.dart';
+import 'package:stax/context/context_git_get_current_branch.dart';
 
 import 'internal_command.dart';
 import 'types_for_internal_command.dart';
@@ -10,9 +13,31 @@ class InternalCommandUpdate extends InternalCommand {
 
   @override
   void run(final List<String> args, Context context) {
-    context.printToConsole("""
+    if (!Platform.isWindows) {
+      context.printToConsole("""
 
 Please refer to the most recent installation instructions in the repository README file for accurate and up-to-date information. You can find the installation section here: https://github.com/TarasMazepa/stax?tab=readme-ov-file#installation
 """);
+      return;
+    }
+    context = context
+        .withScriptPathAsWorkingDirectory()
+        .withRepositoryRootAsWorkingDirectory();
+    final currentBranch = context.getCurrentBranch();
+    final mainBranch = "main";
+    if (currentBranch != mainBranch) {
+      final result = context.git.checkout
+          .arg(mainBranch)
+          .askContinueQuestion(
+              "Switching from '$currentBranch' to '$mainBranch' branch.")
+          ?.announce("Switching to '$mainBranch'.")
+          .runSync()
+          .printNotEmptyResultFields();
+      if (result == null) return;
+    }
+    context.git.pull
+        .announce("Pulling new changes.")
+        .runSync()
+        .printNotEmptyResultFields();
   }
 }
