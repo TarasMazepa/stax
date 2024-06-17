1c2580c7bcd37870f6f18ed6ba5a96bc8bb7d4bc
TarasMazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-06-16 21:19:38 -0700

    Make choco setup even smaller

--- chocolatey/stax/tools/chocolateyinstall.ps1
+++ chocolatey/stax/tools/chocolateyinstall.ps1
@@ -1,42 +1,10 @@
-﻿# $ErrorActionPreference = 'Stop' # stop on all errors
-$toolsDir = "$( Split-Path -parent $MyInvocation.MyCommand.Definition )"
-$appFolder = "$toolsDir\stax"
+﻿$toolsDir = "$( Split-Path -parent $MyInvocation.MyCommand.Definition )"
 
-$appRepoUrl = "https://github.com/TarasMazepa/stax/archive/master.zip"
-$repoZipFile = "$appFolder\repo.zip"
+$repoZipFile = "$toolsDir\repo.zip"
+Get-ChocolateyWebFile -PackageName 'stax' -FileFullPath $repoZipFile -Url 'https://github.com/TarasMazepa/stax/archive/master.zip'
+Get-ChocolateyUnzip -FileFullPath $repoZipFile -Destination $toolsDir
 
-# Create temporary directory for Dart SDK
-if (!(Test-Path $appFolder))
-{
-  New-Item -ItemType Directory -Path $appFolder | Out-Null
-}
+& dart pub --directory="$toolsDir\stax-main\cli" get
+& dart compile exe "$toolsDir\stax-main\cli\bin\cli.dart" -o "$toolsDir\stax.exe"
 
-# Clone App Repository
-Invoke-WebRequest -Uri $appRepoUrl -OutFile $repoZipFile
-
-# Extract
-Expand-Archive -Path $repoZipFile -DestinationPath $appFolder
-
-# Navigate to App Folder
-Set-Location "$appFolder\stax-main\cli"
-
-# Install dart
-#& choco install dart-sdk -y
-#& choco upgrade dart-sdk -y
-
-# Build Dart App
-& dart pub get
-& dart compile exe bin/cli.dart -o stax.exe
-
-# Move binary to user folder
-$userStaxFolder = "$toolsDir\bin"
-if (!(Test-Path $userStaxFolder))
-{
-  New-Item -ItemType Directory -Path $userStaxFolder | Out-Null
-}
-Move-Item -Path "$appFolder\stax-main\cli\stax.exe" -Destination $userStaxFolder -Force
-
-# Cleanup files
-Set-Location $toolsDir
-# Cleanup
-Remove-Item $appFolder -Recurse -Force
+Install-BinFile -Name stax -Path "$toolsDir\stax.exe"
