c316e1c16fed543ba33be4bc9b89947ac64299fb
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-07-04 15:54:26 -0700

    Adds new test case for log (#97)
    Co-authored-by: stax <stax@staxforgit.com>

---
+++ cli/lib/comparison_chain.dart
@@ -0,0 +1,17 @@
+class ComparisonChain {
+  int _result = 0;
+
+  ComparisonChain chain(int Function() compare) {
+    if (_result == 0) _result = compare();
+    return this;
+  }
+
+  ComparisonChain chainBool(bool left, bool right) {
+    int boolToInt(bool x) => x ? 1 : -1;
+    return chain(() => boolToInt(left) - boolToInt(right));
+  }
+
+  int compare() {
+    return _result;
+  }
+}
