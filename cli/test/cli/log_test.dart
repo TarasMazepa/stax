dcc922160b3ce3ba1f38a0ad2e882ae11e572cbd
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-07-04 13:50:56 -0700

    Migrate log test to use log-test-case (#95)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/test/cli/log_test.dart
+++ cli/test/cli/log_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:stax/commit_tree_for_test_case.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
@@ -14,22 +12,11 @@ void main() {
     });
     var commitTree = CommitTreeForTestCase();
     for (int i = 0; i < 3; i++, commitTree = commitTree.next()) {
-      final targetCommands = commitTree.getTargetCommands();
       final targetOutput = commitTree.getTargetOutput();
       final defaultBranch = commitTree.commitName(commitTree.mainId);
-      test(commitTree.commitName(0), () {
-        for (final value in targetCommands) {
-          final parts = value.split(" ");
-          if (parts[0] == "stax") {
-            setup.runLiveStaxSync(parts.sublist(1));
-          } else if (Platform.isWindows && parts[0] == "echo") {
-            setup.runSync("powershell", ["-c", ...parts]);
-          } else if (parts[0] == "echo") {
-            setup.runSync("touch", [parts.last]);
-          } else {
-            setup.runSync(parts[0], parts.sublist(1));
-          }
-        }
+      final commitName = commitTree.commitName(0);
+      test(commitName, () {
+        setup.runLiveStaxSync(["log-test-case", commitName]);
         expect(
             setup.runLiveStaxSync([
               "log",
