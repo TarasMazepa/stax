066167a4a6567c8ecc14b1f7a16c0a3e742bf8fc
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-07-19 11:03:05 -0700

    Changes chocolatey package to check for checksum and fixes previous proken behavior (#160)
    Co-authored-by: stax <stax@staxforgit.com>

--- chocolatey/stax/tools/chocolateyinstall.ps1
+++ chocolatey/stax/tools/chocolateyinstall.ps1
@@ -1,10 +1,21 @@
 ﻿$toolsDir = "$( Split-Path -parent $MyInvocation.MyCommand.Definition )"
 
 $repoZipFile = "$toolsDir\repo.zip"
-Get-ChocolateyWebFile -PackageName 'stax' -FileFullPath $repoZipFile -Url 'https://github.com/TarasMazepa/stax/archive/0.7.30.zip'
-Get-ChocolateyUnzip -FileFullPath $repoZipFile -Destination $toolsDir
 
-& dart pub --directory="$toolsDir\stax-main\cli" get
-& dart compile exe "$toolsDir\stax-main\cli\bin\cli.dart" -o "$toolsDir\stax.exe"
+$tag = "0.6.3"
+
+Get-ChocolateyWebFile `
+    -PackageName 'stax' `
+    -FileFullPath $repoZipFile `
+    -Url "https://github.com/TarasMazepa/stax/archive/$tag.zip" `
+    -Checksum B4C2E8246EA5662D88009085AEFA1FD6957485369ACE872FBB84261FEE47010A `
+    -ChecksumType SHA256
+
+Get-ChocolateyUnzip `
+    -FileFullPath $repoZipFile `
+    -Destination $toolsDir
+
+& dart pub --directory="$toolsDir\stax-$tag\cli" get
+& dart compile exe "$toolsDir\stax-$tag\cli\bin\cli.dart" -o "$toolsDir\stax.exe"
 
 Install-BinFile -Name stax -Path "$toolsDir\stax.exe"
