509b75329e0c10105684f98c48c223bc6c97a5dd
stax <stax@staxforgit.com>
2024-08-29 08:59:39 -0700

    testing lint

--- cli/bin/internal_command.dart
+++ cli/bin/internal_command.dart
@@ -9,13 +9,8 @@ abstract class InternalCommand implements Comparable<InternalCommand> {
   final Map<String, String>? arguments;
   final Map<String, String>? flags;
 
-  const InternalCommand(
-    this.name,
-    this.description, {
-    this.type = InternalCommandType.public,
-    this.arguments,
-    this.flags,
-  });
+  const InternalCommand(this.name, this.description,
+      {this.type = InternalCommandType.public, this.arguments, this.flags});
 
   void run(final List<String> args, final Context context);
 
