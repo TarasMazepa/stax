b66fc5228efa32449a4bd2b41fd8ef68c1f1e394
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-09-01 15:28:33 -0700

    Removes not used code (#287)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/lib/command/internal_command_amend.dart
+++ cli/lib/command/internal_command_amend.dart
@@ -1,8 +1,6 @@
 import 'package:stax/context/context.dart';
 import 'package:stax/context/context_explain_to_user_no_staged_changes.dart';
 import 'package:stax/context/context_git_are_there_staged_changes.dart';
-import 'package:stax/context/context_git_get_child_branches.dart';
-import 'package:stax/context/context_git_get_current_branch.dart';
 import 'package:stax/context/context_git_is_inside_work_tree.dart';
 import 'package:stax/context/context_handle_add_all_flag.dart';
 
@@ -18,10 +16,6 @@ class InternalCommandAmend extends InternalCommand {
 
   @override
   void run(final List<String> args, final Context context) {
-    /**
-     * TODO:
-     *  - Rebase
-     */
     if (context.handleNotInsideGitWorkingTree()) {
       return;
     }
@@ -30,20 +24,10 @@ class InternalCommandAmend extends InternalCommand {
       context.explainToUserNoStagedChanges();
       return;
     }
-    final childBranches = context.getChildBranches();
     context.git.commitAmendNoEdit
         .announce("Amending changes to a commit.")
         .runSync()
         .printNotEmptyResultFields();
-    final rebaseTarget = context.getCurrentBranch() ??
-        context.git.revParseHead
-            .announce("Getting hash of a new commit.")
-            .runSync()
-            .printNotEmptyResultFields();
-    childBranches.removeWhere((e) => e.name == rebaseTarget);
-    if (childBranches.isNotEmpty) {
-      // rebase
-    }
     context.git.pushForce
         .announce("Force pushing to a remote.")
         .runSync()
