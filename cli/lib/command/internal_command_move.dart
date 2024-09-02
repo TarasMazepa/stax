33375fcfa2e5f955ab764f16b272762de28137ef
stax <stax@staxforgit.com>
2024-09-01 17:46:47 -0700

    Adds notes about using first letters instead of full names of commands and directions

--- cli/lib/command/internal_command_move.dart
+++ cli/lib/command/internal_command_move.dart
@@ -29,7 +29,7 @@ class InternalCommandMove extends InternalCommand {
   InternalCommandMove()
       : super(
           "move",
-          "Allows you to move around log tree.",
+          "Allows you to move around log tree. Note: you can type any amount of first letters to specify direction. 'h' instead of 'head', 't' for 'top, 'd' for down, 'u' for 'up', 'b' for 'bottom'",
           arguments: {
             "[arg]+":
                 "up (one up, optionally you can provide followup argument which would be a 0-based index of the child you want to move, by default it is 0), down (one down), top (to the closest top parent that have at least two children or to the top most node, optionally you can provide followup argument which would be a 0-based index of the child you want to move, by default it is 0), bottom (to the closest bottom parent that have at least two children or bottom most node, will stop before any direct parent of <remote>/head), head (<remote>/head)",
