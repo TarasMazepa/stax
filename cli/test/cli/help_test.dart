88c8a4f7ff0807c238302892413945ad2c829955
Alvee <M.Alvee8141@gmail.com>
2024-12-18 13:18:58 +0600

    Update help_test.dart

--- cli/test/cli/help_test.dart
+++ cli/test/cli/help_test.dart
@@ -49,7 +49,6 @@ Note: you can type first letter or couple of first letters instead of full comma
  • move - Allows you to move around log tree. Note: you can type any amount of first letters to specify direction. 'h' instead of 'head', 't' for 'top, 'd' for down, 'u' for 'up', 'b' for 'bottom'
       Positional arguments:
          [arg]+ - up (one up, optionally you can provide followup argument which would be a 0-based index of the child you want to move, by default it is 0), down (one down), top (to the closest top parent that have at least two children or to the top most node, optionally you can provide followup argument which would be a 0-based index of the child you want to move, by default it is 0), bottom (to the closest bottom parent that have at least two children or bottom most node, will stop before any direct parent of <remote>/head), head (<remote>/head)
- • pr - Creates a pull request.
  • pull - Switching to main branch, pull all the changes, deleting gone branches and switching to original branch.
       Flags:
          -f - Force delete gone branches.
