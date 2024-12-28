b115ae71bc36ba1462ce5e7801b1dc84f156cca0
Alvee <M.Alvee8141@gmail.com>
2024-12-28 17:26:01 +0600

    lint fix

--- cli/lib/command/internal_command_settings.dart
+++ cli/lib/command/internal_command_settings.dart
@@ -2,7 +2,6 @@ import 'package:stax/command/internal_command.dart';
 import 'package:stax/command/types_for_internal_command.dart';
 import 'package:stax/context/context.dart';
 import 'package:stax/settings/settings.dart';
-import 'package:stax/settings/string_setting.dart';
 
 class InternalCommandSettings extends InternalCommand {
   static const availableSettings = {
