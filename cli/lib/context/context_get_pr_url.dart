340c269e386df60eba433421f4841b60ceaa1f32
Alvee <M.Alvee8141@gmail.com>
2024-12-29 20:25:46 +0600

    Update context_get_pr_url.dart

--- cli/lib/context/context_get_pr_url.dart
+++ cli/lib/context/context_get_pr_url.dart
@@ -1,10 +1,18 @@
 import 'package:stax/context/context.dart';
-import 'package:stax/context/context_git_get_remote_url.dart';
 
 extension ContextGetPrUrl on Context {
   String? getPrUrl(String targetBranch, String currentBranch) {
-    final remoteUrl = getRemoteUrl();
-    if (remoteUrl == null) return null;
+    final remote = git.remote.runSync().stdout.toString().split('\n')[0].trim();
+    if (remote.isEmpty) return null;
+
+    final remoteUrl = git.remoteGetUrl
+        .arg(remote)
+        .runSync()
+        .stdout
+        .toString()
+        .trim()
+        .replaceFirstMapped(RegExp(r'git@(.*):'), (m) => 'https://${m[1]}/');
+    if (remoteUrl.isEmpty) return null;
 
     return '${remoteUrl.substring(0, remoteUrl.length - 4)}/compare/$targetBranch...$currentBranch?expand=1';
   }
