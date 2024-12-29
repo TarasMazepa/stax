9bee7da9ccdf79ab074b9785820f28d1589818ee
Alvee <M.Alvee8141@gmail.com>
2024-12-29 19:11:36 +0600

    set command

--- cli/lib/command/internal_command_settings.dart
+++ cli/lib/command/internal_command_settings.dart
@@ -8,20 +8,14 @@ class InternalCommandSettings extends InternalCommand {
     'branch_prefix': 'Prefix to add to all new branch names (e.g., "feature/")',
   };
 
-  static const _subcommands = {
-    'show': 'Display current settings',
-    'set': 'Set a setting value',
-    'clear': 'Clear a setting value',
-  };
-
   InternalCommandSettings()
       : super(
           'settings',
           'View or modify stax settings',
           type: InternalCommandType.hidden,
           arguments: {
-            'arg1': 'Subcommand (${_subcommands.keys.join(", ")})',
-            'opt2': 'Setting name (for set/clear)',
+            'arg1': 'Subcommand (set)',
+            'opt2': 'Setting name (for set)',
             'opt3': 'New value (for set)',
           },
         );
@@ -32,60 +26,30 @@ class InternalCommandSettings extends InternalCommand {
     Settings.instance.branchPrefix;
 
     if (args.isEmpty) {
-      _showHelp(context);
-      return;
-    }
-
-    final subcommand = args[0];
-    final remainingArgs = args.skip(1).toList();
-
-    switch (subcommand) {
-      case 'show':
-        _showSettings(context);
-      case 'set':
-        _setSetting(remainingArgs, context);
-      case 'clear':
-        _clearSetting(remainingArgs, context);
-      default:
-        context.printToConsole(
-          'Unknown subcommand: $subcommand\n'
-          'Available subcommands: ${_subcommands.keys.join(", ")}',
-        );
-    }
-  }
-
-  void _showHelp(Context context) {
-    context.printToConsole('Available subcommands:');
-    for (final entry in _subcommands.entries) {
-      context.printToConsole('  ${entry.key}: ${entry.value}');
-    }
-    context.printToConsole('\nAvailable settings:');
-    for (final setting in availableSettings.entries) {
-      context.printToConsole('  ${setting.key}: ${setting.value}');
-    }
-  }
-
-  void _showSettings(Context context) {
-    context.printToConsole('Current settings:');
-    for (final setting in availableSettings.entries) {
-      final currentValue = Settings.instance[setting.key];
-      context.printToConsole(
-        '${setting.key}: ${currentValue ?? "<not set>"}'
-        '\n  ${setting.value}',
-      );
-    }
-  }
-
-  void _setSetting(List<String> args, Context context) {
-    if (args.length < 2) {
       context.printToConsole(
         'Usage: stax settings set <setting_name> <value>',
       );
       return;
     }
 
-    final settingName = args[0];
-    final newValue = args[1];
+    final subcommand = args[0];
+    if (subcommand != 'set') {
+      context.printToConsole(
+        'Unknown subcommand: $subcommand\n'
+        'Available subcommand: set',
+      );
+      return;
+    }
+
+    if (args.length < 3) {
+      context.printToConsole(
+        'Usage: stax settings set <setting_name> <value>',
+      );
+      return;
+    }
+
+    final settingName = args[1];
+    final newValue = args[2];
 
     if (!availableSettings.containsKey(settingName)) {
       context.printToConsole(
@@ -98,28 +62,5 @@ class InternalCommandSettings extends InternalCommand {
     Settings.instance[settingName] = newValue;
     Settings.instance.save();
     context.printToConsole('Updated setting: $settingName = $newValue');
-  }
-
-  void _clearSetting(List<String> args, Context context) {
-    if (args.isEmpty) {
-      context.printToConsole(
-        'Usage: stax settings clear <setting_name>',
-      );
-      return;
-    }
-
-    final settingName = args[0];
-
-    if (!availableSettings.containsKey(settingName)) {
-      context.printToConsole(
-        'Unknown setting: $settingName\n'
-        'Available settings: ${availableSettings.keys.join(", ")}',
-      );
-      return;
-    }
-
-    Settings.instance[settingName] = null;
-    Settings.instance.save();
-    context.printToConsole('Cleared setting: $settingName');
   }
 }
