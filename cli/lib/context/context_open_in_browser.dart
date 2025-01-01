28cfb28830bd409f9b2f290a78c47cf53a111a64
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-01-01 12:40:35 -0800

    Apply suggestions from code review

--- cli/lib/context/context_open_in_browser.dart
+++ cli/lib/context/context_open_in_browser.dart
@@ -3,14 +3,11 @@ import 'dart:io';
 import 'package:stax/context/context.dart';
 
 extension ContextOpenInBrowser on Context {
-  void openInBrowser(String url) {
+  ExternalCommand openInBrowser(String url) {
     final openCommand = Platform.isWindows
         ? ['PowerShell', '-Command', '''& {Start-Process "$url"}''']
         : ['open', url];
 
-    command(openCommand)
-        .announce('Opening URL in browser')
-        .runSync()
-        .printNotEmptyResultFields();
+    return command(openCommand);
   }
 }
