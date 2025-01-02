7b1cbca036960b74346bce083d1fe8db17cf7f0e
Alvee <M.Alvee8141@gmail.com>
2025-01-03 00:15:08 +0600

    PR creation page (#355)
    * Create version.yaml
    
    * Update version.yaml
    
    * PR creation page
    
    * Update help_test.dart
    
    * Update help_test.dart
    
    * Update internal_command_pr_creation.dart
    
    * reuse code
    
    * Update internal_command_commit.dart
    
    * pr redo splitting it into smaller pieces
    
    * refactor code
    
    * Delete version.yaml
    
    ---------
    
    Co-authored-by: Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>

---
+++ cli/lib/command/internal_command_pr_creation.dart
@@ -0,0 +1,60 @@
+import 'package:stax/command/internal_command.dart';
+import 'package:stax/context/context.dart';
+import 'package:stax/context/context_get_pr_url.dart';
+import 'package:stax/context/context_git_get_current_branch.dart';
+import 'package:stax/context/context_git_get_default_branch.dart';
+import 'package:stax/context/context_git_is_inside_work_tree.dart';
+import 'package:stax/context/context_git_log_all.dart';
+import 'package:stax/context/context_open_in_browser.dart';
+
+class InternalCommandPrCreation extends InternalCommand {
+  InternalCommandPrCreation()
+      : super(
+          'pr',
+          'Creates a pull request.',
+        );
+
+  @override
+  void run(final List<String> args, final Context context) {
+    if (context.handleNotInsideGitWorkingTree()) {
+      return;
+    }
+
+    final currentBranch = context.getCurrentBranch();
+    if (currentBranch == null) {
+      context.printToConsole("Can't determine current branch.");
+      return;
+    }
+
+    final root = context.withSilence(true).gitLogAll().collapse();
+    if (root == null) {
+      context.printToConsole("Can't build branch tree.");
+      return;
+    }
+
+    final current = root.findCurrent();
+    if (current == null) {
+      context.printToConsole("Can't find current branch in the tree.");
+      return;
+    }
+
+    final targetBranch = current.parent?.line.branchNameOrCommitHash() ??
+        context.getDefaultBranch();
+    if (targetBranch == null) {
+      context.printToConsole("Can't determine target branch.");
+      return;
+    }
+
+    final prUrl = context.getPrUrl(targetBranch, currentBranch);
+    if (prUrl == null) {
+      context.printToConsole("Can't generate PR URL.");
+      return;
+    }
+
+    context
+        .openInBrowser(prUrl)
+        .announce('Opening PR creation page in browser')
+        .runSync()
+        .printNotEmptyResultFields();
+  }
+}
