3261e913b0a8dc22f83c44578027d8dd2cda17fc
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-01-12 20:13:22 -0800

    Update cli/lib/command/internal_command_amend.dart

--- cli/lib/command/internal_command_amend.dart
+++ cli/lib/command/internal_command_amend.dart
@@ -2,6 +2,7 @@ import 'package:stax/command/flag.dart';
 import 'package:stax/command/internal_command_rebase.dart';
 import 'package:stax/context/context.dart';
 import 'package:stax/context/context_assert_no_conflicting_flags.dart';
+import 'package:stax/context/context_explain_to_user_no_staged_changes.dart';
 import 'package:stax/context/context_git_are_there_staged_changes.dart';
 import 'package:stax/context/context_git_is_inside_work_tree.dart';
 import 'package:stax/context/context_git_log_all.dart';
