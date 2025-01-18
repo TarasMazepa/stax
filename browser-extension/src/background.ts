b24716c44970ce65531e9a353479c8727ae34e8e
Alvee <M.Alvee8141@gmail.com>
2025-01-18 14:00:11 +0600

    :fire: remove github login

--- browser-extension/src/background.ts
+++ browser-extension/src/background.ts
@@ -8,8 +8,5 @@ browser.runtime.onInstalled.addListener((details) => {
 
 // Listen for messages from content script
 browser.runtime.onMessage.addListener(async (message, sender) => {
-  if (message.type === 'GET_AUTH_STATE') {
-    const state = await browser.storage.local.get(['authState']);
-    return state.authState;
-  }
+  
 });
--- browser-extension/src/contentScript.ts
+++ browser-extension/src/contentScript.ts
@@ -4,15 +4,7 @@ console.log('Content script loaded');
 
 // You can add GitHub page-specific functionality here
 async function init() {
-  try {
-    const authState = await browser.runtime.sendMessage({ type: 'GET_AUTH_STATE' });
-    if (authState?.token) {
-      // User is authenticated, you can enhance the GitHub page here
-      console.log('User is authenticated');
-    }
-  } catch (error) {
-    console.error('Error initializing content script:', error);
-  }
+  
 }
 
 init(); 
