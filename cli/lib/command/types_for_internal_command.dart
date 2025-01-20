b7ddde27668a93b5f64833191e92ca4944cfc04d
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-01-19 16:04:13 -0800

    Return advanced prefex matching for command names (#421)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/lib/command/types_for_internal_command.dart
+++ cli/lib/command/types_for_internal_command.dart
@@ -1,1 +1,7 @@
 enum InternalCommandType { public, hidden }
+
+extension InternalCommandTypeIs on InternalCommandType {
+  bool get isPublic => this == InternalCommandType.public;
+
+  bool get isHidden => this == InternalCommandType.hidden;
+}
