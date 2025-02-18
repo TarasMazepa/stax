079a208ffece48a4d867bd9298c6dc4336e3f6b1
Alvee <M.Alvee8141@gmail.com>
2025-02-18 07:45:58 +0600

    Create key_value_list_setting.dart (#461)
    * Create key_value_list_setting.dart
    
    * Fix edge conditions for KeyValueListSetting
    
    ---------
    
    Co-authored-by: stax <stax@staxforgit.com>

---
+++ cli/lib/settings/key_value_list_setting.dart
@@ -0,0 +1,39 @@
+import 'package:collection/collection.dart';
+import 'package:stax/settings/base_list_setting.dart';
+import 'package:stax/settings/settings.dart';
+
+class KeyValueListSetting extends BaseListSetting<MapEntry<String, String>> {
+  KeyValueListSetting(
+    String name,
+    List<MapEntry<String, String>> defaultValue,
+    Settings settings,
+    String description,
+  ) : super(
+          name,
+          defaultValue,
+          settings,
+          description,
+          (s) => switch (s.split('=')) {
+            [final key, final value] => MapEntry(key, value),
+            _ => throw FormatException('Invalid key-value format: $s'),
+          },
+          (entry) => '${entry.key}=${entry.value}',
+        );
+
+  void addKeyValue(String key, String value) {
+    removeByKey(key);
+    add(MapEntry(key, value));
+  }
+
+  void removeByKey(String key) {
+    value = value.where((entry) => entry.key != key).toList();
+  }
+
+  String? getValue(String key) {
+    return value
+        .firstWhereOrNull(
+          (entry) => entry.key == key,
+        )
+        ?.value;
+  }
+}
