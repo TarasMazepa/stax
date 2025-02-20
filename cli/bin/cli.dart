d010c54cdc170321053d40ffca9cb9e4378879fc
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-02-19 17:26:58 -0800

    Update cli.dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -11,7 +11,7 @@ void main(List<String> arguments) {
   arguments = arguments.toList();
   final context = Context.implicit().handleGlobalFlags(arguments);
   switch (arguments) {
-    case []:
+    case [ ]:
       InternalCommandHelp().run([], context);
     case [final commandName, ...final args]:
       final command =
