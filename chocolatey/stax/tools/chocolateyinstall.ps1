cb4733f7fcead89af101b4911843b11b978d6c32
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-02-26 20:21:02 -0800

    Chocolatey update stax to 0.9.44 (#502)
    Co-authored-by: stax <stax@staxforgit.com>

--- chocolatey/stax/tools/chocolateyinstall.ps1
+++ chocolatey/stax/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 
 $toolsDir = "$( Split-Path -Parent $MyInvocation.MyCommand.Definition )"
 
-$tag = '0.9.43'
+$tag = '0.9.44'
 
 Install-ChocolateyZipPackage `
     -PackageName 'stax' `
     -Url "https://github.com/TarasMazepa/stax/archive/$tag.zip" `
-    -Checksum 2ECCF183E43695CC7C200D4B2BE185D3B49BF655D6F7DD676D01A63139D5830E `
+    -Checksum 00FB794AA23131B93BF88024B2C0EB20F48AEAED4A5A1954855811E4480ADE4D `
     -ChecksumType SHA256 `
     -UnzipLocation "$toolsDir"
 
