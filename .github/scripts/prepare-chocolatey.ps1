f9b75a20a0a8cc9ba55e9d15fa94706711434cf1
Alvee <M.Alvee8141@gmail.com>
2025-03-05 09:20:09 +0600

    Chocolatey - bundle binary into nupkg (#514)
    * Chocolatey - bundle binary into nupkg
    
    * Update VERSION
    
    * Apply suggestions from code review
    
    * Update chocolatey-test.yml
    
    * Update chocolatey-test.yml
    
    * Update chocolatey-release.yml
    
    * Update chocolatey-release.yml
    
    * Update chocolatey-test.yml
    
    * Update dart-sdk-versions-trigger.yml
    
    * Update chocolatey-test.yml
    
    * resuable powershell script
    
    * Update chocolatey-release.yml
    
    * Update chocolatey-test.yml
    
    * Update dart-sdk-versions-trigger.yml
    
    * Update dart-sdk-versions-trigger.yml
    
    * Update prepare-chocolatey.ps1
    
    * Update prepare-chocolatey.ps1
    
    * Update prepare-chocolatey.ps1
    
    * Update chocolatey-release.yml
    
    * Update chocolatey-test.yml
    
    * Update chocolatey-release.yml
    
    * Update chocolatey-release.yml
    
    * Update chocolatey-test.yml
    
    ---------
    
    Co-authored-by: Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>

---
+++ .github/scripts/prepare-chocolatey.ps1
@@ -0,0 +1,13 @@
+[xml]$xml = Get-Content -Path "chocolatey/stax/stax.nuspec"
+$version = $xml.package.metadata.version
+Write-Host "Found version: $version"
+
+$downloadUrl = "https://github.com/TarasMazepa/stax/releases/download/$version/windows-x64.zip"
+$outputZip = "windows-x64.zip"
+
+Write-Host "Downloading from: $downloadUrl"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputZip
+
+$ToolsPath = "chocolatey/stax/tools"
+Write-Host "Extracting stax.exe to $ToolsPath"
+Expand-Archive -Path $outputZip -DestinationPath "$ToolsPath" -Force
