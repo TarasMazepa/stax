c4a541e0598afea4edb3ff25a44777b5861ae7ed
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-03-06 10:09:35 -0800

    Delete cli/lib/command/internal_commands_daemon.dart

--- cli/lib/command/internal_commands_daemon.dart
+++
@@ -1,5 +0,0 @@
-import 'package:stax/command/internal_command.dart';
-import 'package:stax/command/internal_command_exit.dart';
-
-final List<InternalCommand> internalCommandsDaemon = [InternalCommandExit()]
-  ..sort();
