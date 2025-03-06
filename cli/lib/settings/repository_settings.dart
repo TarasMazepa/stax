15fb1d13178b26d53f256b701c0e624d9214e013
Alvee <M.Alvee8141@gmail.com>
2025-03-06 19:10:11 +0600

    Capture rebase data and relevant flags into file in git repository

--- cli/lib/settings/repository_settings.dart
+++ cli/lib/settings/repository_settings.dart
@@ -2,20 +2,23 @@ import 'package:path/path.dart' as path;
 import 'package:stax/context/context.dart';
 import 'package:stax/context/context_git_get_repository_root.dart';
 import 'package:stax/settings/base_settings.dart';
+import 'package:stax/settings/rebase_data_setting.dart';
 
 class RepositorySettings extends BaseSettings {
+  late final RebaseDataSetting rebaseData = RebaseDataSetting(
+    'rebase_data',
+    null,
+    this,
+    'Data for resumable rebase operations',
+  );
+
   static RepositorySettings? load(Context context) {
     final root = context.getRepositoryRoot();
     if (root == null) return null;
-    return RepositorySettings(
-      path.join(
-        Uri.parse(root).toFilePath(),
-        '.git',
-        'info',
-        'stax',
-        'settings.json',
-      ),
-    );
+
+    final settingsPath =
+        path.join(root, '.git', 'info', 'stax', 'settings.json');
+    return RepositorySettings(settingsPath);
   }
 
   RepositorySettings(super.path) : super.fromPath();
