38e5227c3e8ea5915ac5758538fab5b45666cdfc
Alvee <M.Alvee8141@gmail.com>
2025-03-07 00:50:21 +0600

    stax daemon (#527)
    * stax daemon
    
    * Format dart code in stax-daemon branch at 2025-03-06T17-10-22 (#529)
    
    Co-authored-by: stax <stax@staxforgit.com>
    
    * Delete cli/lib/command/internal_command_exit.dart
    
    * Delete cli/lib/command/internal_commands_daemon.dart
    
    * Update stax_daemon.dart
    
    * Update stax_daemon.dart
    
    * Update stax_daemon.dart
    
    * Update stax_daemon.dart
    
    ---------
    
    Co-authored-by: Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
    Co-authored-by: stax <stax@staxforgit.com>

---
+++ cli/bin/stax_daemon.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  final daemonPort = 5000;
+  print('Starting stax daemon on port $daemonPort...');
+
+  ServerSocket? serverSocket;
+  try {
+    serverSocket = await ServerSocket.bind(
+      InternetAddress.loopbackIPv4,
+      daemonPort,
+    );
+
+    bool isRunning = true;
+    serverSocket.listen((client) {
+      client.listen((data) {
+        switch (String.fromCharCodes(data).trim()) {
+          case 'exit':
+            isRunning = false;
+            break;
+        }
+      }, onDone: () => client.close());
+    });
+
+    while (isRunning) {
+      await Future.delayed(Duration(seconds: 1));
+    }
+  } finally {
+    await serverSocket?.close();
+    print('Daemon shut down');
+  }
+}
