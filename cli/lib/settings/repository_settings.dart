e92ee8c3c414affaa9bfc497be0ad32d5f9b8a59
Alvee <M.Alvee8141@gmail.com>
2025-03-07 19:23:47 +0600

    review changes

--- cli/lib/settings/repository_settings.dart
+++ cli/lib/settings/repository_settings.dart
@@ -2,16 +2,8 @@ import 'package:path/path.dart' as path;
 import 'package:stax/context/context.dart';
 import 'package:stax/context/context_git_get_repository_root.dart';
 import 'package:stax/settings/base_settings.dart';
-import 'package:stax/settings/rebase_data_setting.dart';
 
 class RepositorySettings extends BaseSettings {
-  late final RebaseDataSetting rebaseData = RebaseDataSetting(
-    'rebase_data',
-    null,
-    this,
-    'Data for resumable rebase operations',
-  );
-
   static RepositorySettings? load(Context context) {
     final root = context.getRepositoryRoot();
     if (root == null) return null;
