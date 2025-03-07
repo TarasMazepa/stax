957c5968112c6915f25c99b78be1dd4bcde3abc9
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-03-07 06:44:41 -0800

    Format dart code in resumable-rebase-pr-1 branch at 2025-03-07T14-44-36 (#537)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/lib/command/internal_command_rebase.dart
+++ cli/lib/command/internal_command_rebase.dart
@@ -22,15 +22,14 @@ class InternalCommandRebase extends InternalCommand {
   );
 
   InternalCommandRebase()
-      : super(
-          'rebase',
-          'rebase tree of branches on top of main',
-          arguments: {
-            'opt1':
-                'Optional argument for target, will default to <remote>/HEAD',
-          },
-          flags: [theirsFlag, oursFlag],
-        );
+    : super(
+        'rebase',
+        'rebase tree of branches on top of main',
+        arguments: {
+          'opt1': 'Optional argument for target, will default to <remote>/HEAD',
+        },
+        flags: [theirsFlag, oursFlag],
+      );
 
   @override
   void run(List<String> args, Context context) {
@@ -59,9 +58,10 @@ class InternalCommandRebase extends InternalCommand {
 
     final userProvidedTarget = args.elementAtOrNull(0);
 
-    final GitLogAllNode? targetNode = userProvidedTarget != null
-        ? root.findAnyRefThatEndsWith(userProvidedTarget)
-        : root.findRemoteHead();
+    final GitLogAllNode? targetNode =
+        userProvidedTarget != null
+            ? root.findAnyRefThatEndsWith(userProvidedTarget)
+            : root.findRemoteHead();
 
     if (targetNode == null) {
       context.printToConsole("Can't find target branch.");
@@ -77,20 +77,24 @@ class InternalCommandRebase extends InternalCommand {
 
     final branchesForRebase = current.localBranchNamesInOrderForRebase();
 
-    final branches = branchesForRebase
-        .map((node) => {'node': node.node, 'parent': node.parent ?? ''})
-        .toList();
+    final branches =
+        branchesForRebase
+            .map((node) => {'node': node.node, 'parent': node.parent ?? ''})
+            .toList();
 
     final rebaseData = RebaseData(
       hasTheirsFlag: hasTheirsFlag,
       hasOursFlag: hasOursFlag,
       rebaseOnto: rebaseOnto,
-      steps: branches
-          .map((branch) => RebaseStep(
-                node: branch['node'] as String,
-                parent: branch['parent'] as String,
-              ))
-          .toList(),
+      steps:
+          branches
+              .map(
+                (branch) => RebaseStep(
+                  node: branch['node'] as String,
+                  parent: branch['parent'] as String,
+                ),
+              )
+              .toList(),
     );
 
     final rebaseFile = File('.git/info/stax/rebase.json');
@@ -100,17 +104,18 @@ class InternalCommandRebase extends InternalCommand {
     bool changeParentOnce = true;
 
     for (var node in branchesForRebase) {
-      final exitCode = context.git.rebase
-          .args([
-            if (hasTheirsFlag) ...['-X', 'theirs'],
-            if (hasOursFlag) ...['-X', 'ours'],
-            if (changeParentOnce) rebaseOnto else node.parent!,
-            node.node,
-          ])
-          .announce()
-          .runSync()
-          .printNotEmptyResultFields()
-          .exitCode;
+      final exitCode =
+          context.git.rebase
+              .args([
+                if (hasTheirsFlag) ...['-X', 'theirs'],
+                if (hasOursFlag) ...['-X', 'ours'],
+                if (changeParentOnce) rebaseOnto else node.parent!,
+                node.node,
+              ])
+              .announce()
+              .runSync()
+              .printNotEmptyResultFields()
+              .exitCode;
       changeParentOnce = false;
       if (exitCode != 0) {
         context.printParagraph('Rebase failed');
