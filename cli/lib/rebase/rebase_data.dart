20ef6f26cadccc28dde6b0f39c48cec00b2f600e
Alvee <M.Alvee8141@gmail.com>
2025-03-12 00:23:21 +0600

    Resumable Rebase PR-1 define rebase data class

---
+++ cli/lib/rebase/rebase_data.dart
@@ -0,0 +1,35 @@
+import 'package:stax/rebase/rebase_step.dart';
+
+class RebaseData {
+  final bool hasTheirsFlag;
+  final bool hasOursFlag;
+  final String rebaseOnto;
+  final List<RebaseStep> steps;
+  int currentIndex;
+
+  RebaseData(
+    this.hasTheirsFlag,
+    this.hasOursFlag,
+    this.rebaseOnto,
+    this.steps,
+    this.currentIndex,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'hasTheirsFlag': hasTheirsFlag,
+    'hasOursFlag': hasOursFlag,
+    'rebaseOnto': rebaseOnto,
+    'branches': steps.map((step) => step.toJson()).toList(),
+    'currentIndex': currentIndex,
+  };
+
+  factory RebaseData.fromJson(Map<String, dynamic> json) {
+    return RebaseData(
+      json['hasTheirsFlag'] as bool,
+      json['hasOursFlag'] as bool,
+      json['rebaseOnto'] as String,
+      (json['branches'] as List).map((e) => RebaseStep.fromJson(e)).toList(),
+      json['currentIndex'] as int,
+    );
+  }
+}
