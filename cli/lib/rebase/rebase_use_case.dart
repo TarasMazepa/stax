ce1e3dcb4ebe0ab4cd6fde832e3e685818179ef3
stax <stax@staxforgit.com>
2025-03-10 20:51:47 -0700

    Refactoring

---
+++ cli/lib/rebase/rebase_use_case.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:stax/context/context.dart';
+import 'package:stax/context/context_git_get_repository_root.dart';
+import 'package:stax/rebase/rebase_data.dart';
+
+class RebaseUseCase {
+  final Context _context;
+  RebaseData? _rebaseData;
+
+  RebaseData? get rebaseDate => _rebaseData;
+
+  static RebaseUseCase? create(Context context) {
+    final repositoryRoot = context.withSilence(true).getRepositoryRoot();
+    if (repositoryRoot == null) return null;
+    final file = File(
+      path.join(
+        context.withSilence(true).getRepositoryRoot()!,
+        '.git',
+        'info',
+        'stax',
+        'rebase.json',
+      ),
+    );
+    if (!file.existsSync()) {
+      return RebaseUseCase(context, null);
+    }
+    RebaseData? rebaseData;
+    for (int i = 0; i < 3; i++) {
+      try {
+        rebaseData = RebaseData.fromJson(jsonDecode(file.readAsStringSync()));
+        break;
+      } catch (e) {
+        try {
+          file.deleteSync();
+        } catch (e) {
+          // no op
+        }
+      }
+    }
+    return RebaseUseCase(context, rebaseData);
+  }
+
+  RebaseUseCase(this._context, this._rebaseData);
+}
