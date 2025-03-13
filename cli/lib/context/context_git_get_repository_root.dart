882305ead0928a69100f58ea8c460b2c6de1e34d
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-03-13 15:49:26 -0700

    Make Context.getRepositoryRoot() result cachable (#550)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/lib/context/context_git_get_repository_root.dart
+++ cli/lib/context/context_git_get_repository_root.dart
@@ -1,14 +1,20 @@
 import 'package:stax/context/context.dart';
 
 extension ContextGitGetRepositoryRoot on Context {
+  static String? _repositoryRoot;
+  static bool _loadedRepositoryRoot = false;
+
   String? getRepositoryRoot() {
-    return git.revParseShowTopLevel
-        .announce('Getting top level location of repository.')
-        .runSync()
-        .printNotEmptyResultFields()
-        .assertSuccessfulExitCode()
-        ?.stdout
-        .toString()
-        .trim();
+    if (_loadedRepositoryRoot) return _repositoryRoot;
+    _loadedRepositoryRoot = true;
+    return _repositoryRoot =
+        git.revParseShowTopLevel
+            .announce('Getting top level location of repository.')
+            .runSync()
+            .printNotEmptyResultFields()
+            .assertSuccessfulExitCode()
+            ?.stdout
+            .toString()
+            .trim();
   }
 }
