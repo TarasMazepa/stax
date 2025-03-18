7ae7f88d90a0a68ed9b153619b5715df6a1fce7c
stax <stax@staxforgit.com>
2025-03-18 19:47:33 +0000

    Update cli/lib/command/internal_command_version.dart to have 0.9.49 as a default version

--- cli/lib/command/internal_command_version.dart
+++ cli/lib/command/internal_command_version.dart
@@ -5,7 +5,7 @@ import 'internal_command.dart';
 class InternalCommandVersion extends InternalCommand {
   static final version = const String.fromEnvironment(
     'version',
-    defaultValue: '0.9.48',
+    defaultValue: '0.9.49',
   );
 
   InternalCommandVersion() : super('version', 'Version of stax');
