a0d65319259cbfbeb90a72b9f46ef52afed0ef29
Alvee <M.Alvee8141@gmail.com>
2025-05-07 21:24:06 +0600

    Update internal_command_log.dart

--- cli/lib/command/internal_command_log.dart
+++ cli/lib/command/internal_command_log.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:stax/command/flag.dart';
 import 'package:stax/context/context.dart';
+import 'package:stax/context/context_git_get_repository_root.dart';
 import 'package:stax/context/context_git_is_inside_work_tree.dart';
 import 'package:stax/context/context_git_log_all.dart';
 import 'package:stax/log/decorated/decorated_log_line_producer.dart';
+import 'package:path/path.dart' as path;
 
 import 'internal_command.dart';
 
@@ -18,15 +23,21 @@ class InternalCommandLog extends InternalCommand {
     description: 'show remote branches also',
   );
 
+  static final Flag watchFlag = Flag(
+    short: '-w',
+    long: '--watch',
+    description: 'watch all branches',
+  );
+
   InternalCommandLog()
     : super(
         'log',
         'Shows a tree of all branches.',
-        flags: [defaultBranchFlag, allBranchesFlag],
+        flags: [defaultBranchFlag, allBranchesFlag, watchFlag],
       );
 
   @override
-  void run(List<String> args, Context context) {
+  void run(List<String> args, Context context) async {
     if (context.handleNotInsideGitWorkingTree()) {
       return;
     }
@@ -42,6 +53,7 @@ class InternalCommandLog extends InternalCommand {
     }
 
     final showAllBranches = allBranchesFlag.hasFlag(args);
+    final shouldWatch = watchFlag.hasFlag(args);
 
     print(
       materializeDecoratedLogLines(
@@ -52,5 +64,66 @@ class InternalCommandLog extends InternalCommand {
         ),
       ).join('\n'),
     );
+
+    if (!shouldWatch) return;
+    final root = context.withSilence(true).getRepositoryRoot();
+
+    if (root == null) return;
+    print(context.getRepositoryRoot());
+    final gitHead = path.join(
+      Uri.directory(root, windows: Platform.isWindows).toFilePath(),
+      '.git',
+      'HEAD',
+    );
+    final gitRefs = path.join(
+      Uri.directory(root, windows: Platform.isWindows).toFilePath(),
+      '.git',
+      'refs',
+    );
+    final gitObjects = path.join(
+      Uri.directory(root, windows: Platform.isWindows).toFilePath(),
+      '.git',
+      'objects',
+    );
+
+    final watchList = [
+      File(gitHead),
+      Directory(gitRefs),
+      Directory(gitObjects),
+    ];
+
+    for (final fileEntity in watchList) {
+      if (await fileEntity.exists()) {
+        if (fileEntity is File) {
+          final parentDir = fileEntity.parent;
+          final fileName = fileEntity.uri.pathSegments.last;
+
+          parentDir.watch(recursive: true).listen((event) {
+            if (event.path.endsWith(fileName)) {
+              runStaxLog(context);
+            }
+          });
+        } else if (fileEntity is Directory) {
+          fileEntity.watch(recursive: true).listen((event) {
+            runStaxLog(context);
+          });
+        }
+      }
+    }
+  }
+
+  void runStaxLog(Context context) async {
+    print(Process.runSync('clear', [], runInShell: true).stdout);
+
+    final result = context
+        .command(['stax', 'log'])
+        .runSync(
+          runInShell: true,
+          stdoutEncoding: const Utf8Codec(),
+          stderrEncoding: const Utf8Codec(),
+        );
+    if (result.stdout != null) {
+      print(result.stdout);
+    }
   }
 }
