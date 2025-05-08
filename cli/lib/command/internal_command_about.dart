c0fbfbd23cf5f673c1d726e5cc0697647d9b3750
Alvee <M.Alvee8141@gmail.com>
2025-05-08 20:48:30 +0600

    feat: stax about command

---
+++ cli/lib/command/internal_command_about.dart
@@ -0,0 +1,23 @@
+import 'package:stax/command/internal_command.dart';
+import 'package:stax/context/context.dart';
+
+class InternalCommandAbout extends InternalCommand {
+  InternalCommandAbout()
+    : super('about', 'Shows information about the current project.');
+
+  @override
+  void run(final List<String> args, Context context) {
+    context.printToConsole('stax - A Git workflow tool');
+    context.printToConsole('');
+    context.printToConsole(
+      'stax is a command-line tool that helps you manage your Git workflow more efficiently.',
+    );
+    context.printToConsole(
+      'It provides a set of commands to simplify common Git operations and maintain a clean repository structure.',
+    );
+    context.printToConsole('');
+    context.printToConsole(
+      'For more information, visit: https://staxforgit.com/',
+    );
+  }
+}
