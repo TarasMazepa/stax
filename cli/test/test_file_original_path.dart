7c3f313414acb4f8cefaabf32204d7ccaad26922
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-06-05 20:38:18 -0700

    getTestFileOriginalPath() - normalize path (#741)
    Co-authored-by: Taras Mazepa <git@okl.pw>

--- cli/test/test_file_original_path.dart
+++ cli/test/test_file_original_path.dart
@@ -1,9 +1,19 @@
-String? getTestFileOriginalPath() {
+String? getTestFileUriString() {
   return RegExp(
     r'#.* +main.*\((.*):\d+:\d+\)',
   ).firstMatch(StackTrace.current.toString())?.group(1);
 }
 
-String assertTestFileOriginalPath() {
-  return getTestFileOriginalPath()!;
+String assertTestFileUriString() {
+  return getTestFileUriString()!;
+}
+
+String? getTestFilePath() {
+  final uriString = getTestFileUriString();
+  if (uriString == null) return null;
+  return Uri.tryParse(uriString)?.toFilePath();
+}
+
+String assertTestFilePath() {
+  return getTestFilePath()!;
 }
