7d9cf8d45a957d9ff0dd5f6a09a04c05f94d0a75
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-06-26 13:17:42 -0700

    Format dart code in tmazepa/0 branch (#778)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/test/git/branch_info_test.dart
+++ cli/test/git/branch_info_test.dart
@@ -3,11 +3,18 @@ import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  test('  rchan2-attachmentinvalidURL ccf7ce2d606 [origin/rchan2/attachmentinvalidURL: gone] Initial', () {
-    final branch = '  rchan2-attachmentinvalidURL ccf7ce2d606 [origin/rchan2/attachmentinvalidURL: gone] Initial';
-    final branchInfo = BranchInfo.parse(branch);
-    expect(branchInfo.toString(), 'rchan2-attachmentinvalidURL ccf7ce2d606 origin/rchan2/attachmentinvalidURL gone Initial');
-    expect(branchInfo.gone, true);
-    expect(branchInfo.commitHash, 'ccf7ce2d606');
-  });
+  test(
+    '  rchan2-attachmentinvalidURL ccf7ce2d606 [origin/rchan2/attachmentinvalidURL: gone] Initial',
+    () {
+      final branch =
+          '  rchan2-attachmentinvalidURL ccf7ce2d606 [origin/rchan2/attachmentinvalidURL: gone] Initial';
+      final branchInfo = BranchInfo.parse(branch);
+      expect(
+        branchInfo.toString(),
+        'rchan2-attachmentinvalidURL ccf7ce2d606 origin/rchan2/attachmentinvalidURL gone Initial',
+      );
+      expect(branchInfo.gone, true);
+      expect(branchInfo.commitHash, 'ccf7ce2d606');
+    },
+  );
 }
