69322c1b2f063b00471621e83be63254e42e1438
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-07-13 21:22:33 -0700

    Update cli/lib/command/internal_command_version.dart to have 0.10.12 as a default version (#848)
    Co-authored-by: stax <stax@staxforgit.com>

--- cli/lib/command/internal_command_version.dart
+++ cli/lib/command/internal_command_version.dart
@@ -5,7 +5,7 @@ import 'internal_command.dart';
 class InternalCommandVersion extends InternalCommand {
   static final version = const String.fromEnvironment(
     'version',
-    defaultValue: '0.10.11',
+    defaultValue: '0.10.12',
   );
 
   InternalCommandVersion() : super('version', 'Version of stax');
