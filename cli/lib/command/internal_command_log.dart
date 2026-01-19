3806bd04b861cc38ad7dea3e389f61d9770738bb
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2026-01-19 10:37:10 -0800

    Adding more lint rules and fixing some issues (#991)
    Co-authored-by: Taras Mazepa <git@okl.pw>

--- cli/lib/command/internal_command_log.dart
+++ cli/lib/command/internal_command_log.dart
@@ -1,10 +1,9 @@
 import 'package:stax/command/flag.dart';
+import 'package:stax/command/internal_command.dart';
 import 'package:stax/context/context.dart';
 import 'package:stax/context/context_git_is_inside_work_tree.dart';
 import 'package:stax/context/context_git_log_all.dart';
 import 'package:stax/log/decorated/decorated_log_line_producer.dart';
-
-import 'internal_command.dart';
 
 class InternalCommandLog extends InternalCommand {
   static final Flag defaultBranchFlag = Flag(
