639de0a1116745bf8a6279e7df34124890e5f637
vijay_kbs <kindlebit.mobile@gmail.com>
2026-07-07 14:00:14 +0530

    Skip e2e2 interactive test on Windows

--- cli/test/e2e2/interactive_smoke_test.dart
+++ cli/test/e2e2/interactive_smoke_test.dart
@@ -1,8 +1,9 @@
 import 'package:test/test.dart';
+
 import 'base/e2e2_group.dart';
 
 void main() {
-  e2e2Group('interactive session', onPlatform: const {}, (containerGetter) {
+  e2e2Group('interactive session', (containerGetter) {
     test('streams TTY output and accepts input while running', () async {
       final session = await containerGetter().execInteractive([
         'sh',
